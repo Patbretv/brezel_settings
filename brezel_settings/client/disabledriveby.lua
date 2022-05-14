61909c5b0bf2e57f9efbef92c8f1f70a2cc1f78b
Patbretv <83968864+Patbretv@users.noreply.github.com>
2022-05-14 08:37:34 +0200

    Update disabledriveby.lua

--- brezel_settings/client/disabledriveby.lua
+++ brezel_settings/client/disabledriveby.lua
@@ -1,13 +1,10 @@
-local passengerDriveBy = true
-
 Citizen.CreateThread(function()
+	if not Config.DisableDriveBy then return end
 	while true do
 		Wait(1)
-
-		playerPed = GetPlayerPed(-1)
-		car = GetVehiclePedIsIn(playerPed, false)
+		local playerPed = PlayerPedId()
+		local car = GetVehiclePedIsIn(playerPed, false)
 		if car then
-            if Config.DisableDriveBy then
 			if GetPedInVehicleSeat(car, -1) == playerPed then
 				SetPlayerCanDoDriveBy(PlayerId(), false)
 			elseif passengerDriveBy then
@@ -15,9 +12,10 @@ Citizen.CreateThread(function()
 			else
 				SetPlayerCanDoDriveBy(PlayerId(), false)
 			end
+		else
+			Citizen.Wait(500) --no point running fast while driving
 		end
 	end
-end
 end)
 
 print("Brezel Serivces dsc.gg/brezelservices")
